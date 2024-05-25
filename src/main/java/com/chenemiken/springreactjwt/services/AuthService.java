238dd5a9a20be67a73971ca9ee0fecb02762131a
ChenemiKen <chenemiken15@gmail.com>
2024-05-25 10:31:28 +0100

    setup signup

--- src/main/java/com/chenemiken/springreactjwt/services/AuthService.java
+++ src/main/java/com/chenemiken/springreactjwt/services/AuthService.java
@@ -1,3 +1,11 @@
 package com.chenemiken.springreactjwt.services;
 
-public class AuthService {}
+import com.chenemiken.springreactjwt.models.requests.SignupRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+  public void signup(SignupRequest request){
+    
+  }
+}
